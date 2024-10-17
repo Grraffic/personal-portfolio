dd5c85933a57b7f9c27bd7415b1a8160709b6162
Rafael Ramos <ramosraf278@gmail.com>
2024-10-18 02:40:23 +0800

    Final Project

--- src/App.jsx
+++ src/App.jsx
@@ -489,6 +489,36 @@ function App() {
                     </figure>
                   </div>
                 </div>
+                <div className="col-lg-4 col-md-6 portfolio-item web-des">
+                  <div className="portfolio-wrap">
+                    <figure>
+                      <img
+                        src="./assets/img/Daniels.png"
+                        className="img-fluid"
+                        alt=""
+                      />
+                      <a
+                        href="./assets/img/Daniels.png"
+                        className="link-preview"
+                        data-lightbox="portfolio"
+                        data-title="Project Name"
+                        title="Preview"
+                      >
+                        <i className="fa fa-eye" />
+                      </a>
+                      <a
+                        href="https://www.figma.com/proto/kJZQ7lmNOEFWeN7BkEWuXn/Untitled?node-id=0-1&t=IZUjyszG77a1xFMk-1"
+                        className="link-details"
+                        title="More Details"
+                      >
+                        <i className="fa fa-link" />
+                      </a>
+                      <a className="portfolio-title" href="#">
+                        Daniel's Coffee Website <span>Web Design</span>
+                      </a>
+                    </figure>
+                  </div>
+                </div>
                 <div className="col-lg-4 col-md-6 portfolio-item web-dev">
                   <div className="portfolio-wrap">
                     <figure>
